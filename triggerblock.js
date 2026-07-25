2af5efd3fa5a9cc05559731a4e714605e2d92c90
AleksaMilosevic07 <007milosevicaleksa@gmail.com>
2026-07-25 08:43:52 +0200

    Fear descriptions and unsaved changes warning

--- triggerblock.js
+++ triggerblock.js
@@ -1,8 +1,16 @@
 const fearForm = document.getElementById("fear-form");
+let changeWarning = document.getElementById("changeWarning");
+
+changeWarning.style.display = "none";  // Hide on start
 
 fearForm.addEventListener("submit", function(e){
     e.preventDefault();
     const formData = new FormData(fearForm);
-    let phobias = formData = formData.getAll("phobias");
-    console.log(phobias);
-})
+    let phobias = formData.getAll("phobias");
+    //! Save preferences
+});
+
+fearForm.addEventListener("change", function(e){
+    //! If selected is different then what is saved, show the warning
+    changeWarning.style.display = "block"
+});
--- triggermenu/menu.js
+++ triggermenu/menu.js
@@ -1,13 +1,15 @@
-console.log("test");
 import fears from '../fears.json' with { type: 'json' };
 
 const fearForm = document.getElementById("fear-options");
 
 for(let i = 0; i < fears.length; i++)
 {
+    // Label
     let fear = document.createElement("label");
     fear.innerText = `${fears[i].name}`;
     fearForm.appendChild(fear);
+    
+    // Checkbox
     let fearCheck = document.createElement("input")
     fearCheck.type = "checkbox";
     fearCheck.id = fears[i].name;
@@ -16,4 +18,21 @@ for(let i = 0; i < fears.length; i++)
     
     let br = document.createElement("br");
     fearForm.appendChild(br);
+    
+    // Description
+    let fearDesc = document.createElement("p");
+    fearDesc.innerHTML = `${fears[i].desc}`;
+    fearDesc.hidden = true;
+    fearForm.appendChild(fearDesc);
+    
+    // Event listener
+    fearCheck.addEventListener("click", function(e){
+        if(fearCheck.checked)
+        {
+            fearDesc.hidden = false;
+        }
+        else fearDesc.hidden = true;
+    });
 }
+
+
