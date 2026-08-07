26f5e9b67c98024010a08e18d6df5b82e1939ab5
AleksaMilosevic07 <007milosevicaleksa@gmail.com>
2026-08-07 16:10:55 +0200

    Expanded phobia options

--- triggerblock.js
+++ triggerblock.js
@@ -1,7 +1,9 @@
 let activeFears = [];
 
 const selectedFears = browser.storage.local.get("fears");
-selectedFears.then((result) => {
-    activeFears = result.fears;
+selectedFears.then((results) => {
+    activeFears = results.fears;
     console.log(`Fears: ${activeFears}`);
 });
+
+
